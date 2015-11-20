44027e31a146d9220cd6e2af754d7f1d5b269cbb
Tom Herold <tom.herold@scalableminds.com>
2015-11-20 13:02:23 +0100

    added lib folder

---
+++ client/scripts/lib/router.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { createHistory, useBasename } from "history"
+import { Router, Route, IndexRoute, DefaultRoute, NotFoundRoute } from "react-router";
+
+import Component from "../components/baseComponent.jsx";
+import Home from "../pages/home.jsx";
+import NotFound from "../pages/notFound.jsx";
+
+class AppRouter extends Component {
+
+  render() {
+
+    const history = useBasename(createHistory)({
+      basename : "/huge-apps"
+    })
+
+    return (
+      <Router history={history}>
+        <Route name="app" path="/" component={ Home }>
+          <IndexRoute component={Home} />
+          <Route path="*" component={ NotFound } />
+        </Route>
+      </Router>
+    );
+  }
+}
+
+export default AppRouter;
